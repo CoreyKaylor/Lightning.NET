4d9a7641774ad6eb9a9116cb1e09bb28be6cabb1
Ilya Lukyanov <ilya.lukyanov@gmail.com>
2015-01-29 22:43:46 +1300

    Added missing to bytes converters
    Closes #42

--- src/LightningDB/Converters/DefaultConverters.cs
+++ src/LightningDB/Converters/DefaultConverters.cs
@@ -14,27 +14,41 @@ namespace LightningDB.Converters
         public void RegisterDefault(LightningEnvironment environment)
         {
             var store = environment.ConverterStore;
-            store.AddConvertToBytes<int>((db, x) => BitConverter.GetBytes(x));
-            store.AddConvertToBytes<string>((db, x) => db.Encoding.GetBytes(x));
+
+            store.AddConvertToBytes<Guid>((db, x) => x.ToByteArray());
+            store.AddConvertToBytes<Double>((db, x) => BitConverter.GetBytes(x));
+            store.AddConvertToBytes<Single>((db, x) => BitConverter.GetBytes(x));
+            store.AddConvertToBytes<Boolean>((db, x) => BitConverter.GetBytes(x));
+            store.AddConvertToBytes<Int16>((db, x) => BitConverter.GetBytes(x));
+            store.AddConvertToBytes<Int32>((db, x) => BitConverter.GetBytes(x));
+            store.AddConvertToBytes<Int64>((db, x) => BitConverter.GetBytes(x));
+            store.AddConvertToBytes<UInt16>((db, x) => BitConverter.GetBytes(x));
+            store.AddConvertToBytes<UInt32>((db, x) => BitConverter.GetBytes(x));
+            store.AddConvertToBytes<UInt64>((db, x) => BitConverter.GetBytes(x));
+            store.AddConvertToBytes<Char>((db, x) => BitConverter.GetBytes(x));
+            store.AddConvertToBytes<Byte>((db, x) => new [] { x });
+            store.AddConvertToBytes<SByte>((db, x) => new[] { (byte) x });
+            store.AddConvertToBytes<String>((db, x) => db.Encoding.GetBytes(x));
             store.AddConvertToBytes<byte[]>((db, x) => x);
             
-            convertFromBytesWithCorrectSize(store, (db, x) => new Guid(x), 16);
-            convertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToDouble(x, 0));
-            convertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToSingle(x, 0));
-            convertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToBoolean(x, 0));
-            convertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToInt16(x, 0));
-            convertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToInt32(x, 0));
-            convertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToInt64(x, 0));
-            convertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToUInt16(x, 0));
-            convertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToUInt32(x, 0));
-            convertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToUInt64(x, 0));
-            convertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToChar(x, 0));
-            convertFromBytesWithCorrectSize(store, (db, x) => x[0]);
-            convertFromBytesWithCorrectSize(store, (db, x) => (SByte)x[0]);
+            ConvertFromBytesWithCorrectSize(store, (db, x) => new Guid(x), 16);
+            ConvertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToDouble(x, 0));
+            ConvertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToSingle(x, 0));
+            ConvertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToBoolean(x, 0));
+            ConvertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToInt16(x, 0));
+            ConvertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToInt32(x, 0));
+            ConvertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToInt64(x, 0));
+            ConvertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToUInt16(x, 0));
+            ConvertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToUInt32(x, 0));
+            ConvertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToUInt64(x, 0));
+            ConvertFromBytesWithCorrectSize(store, (db, x) => BitConverter.ToChar(x, 0));
+            ConvertFromBytesWithCorrectSize(store, (db, x) => x[0]);
+            ConvertFromBytesWithCorrectSize(store, (db, x) => (SByte)x[0]);            
             store.AddConvertFromBytes((db, x) => db.Encoding.GetString(x));
+            store.AddConvertFromBytes((db, x) => x);
         }
 
-        private void convertFromBytesWithCorrectSize<TTo>(ConverterStore store, Func<LightningDatabase, byte[], TTo> convert, int? size = null) where TTo : struct
+        private void ConvertFromBytesWithCorrectSize<TTo>(ConverterStore store, Func<LightningDatabase, byte[], TTo> convert, int? size = null) where TTo : struct
         {
             var func = convert.EnsureCorrectSize(size);
             store.AddConvertFromBytes(func);
