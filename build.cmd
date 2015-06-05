a63823fb4c5c5ea9982ce4bea2f05f1f2d363290
Corey Kaylor <corey@kaylors.net>
2015-06-05 08:17:03 -0600

    Added build script and appveyor config

---
+++ build.cmd
@@ -0,0 +1,28 @@
+@echo off
+
+pushd %~dp0
+set "DNX_NUGET_API_URL=https://www.nuget.org/api/v2"
+setlocal EnableDelayedExpansion 
+where dnvm
+if %ERRORLEVEL% neq 0 (
+    @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+    set PATH=!PATH!;!userprofile!\.k\bin
+    set KRE_HOME=!USERPROFILE!\.k
+    goto install
+)
+
+:install
+call dnvm install 1.0.0-beta4
+call dnvm use 1.0.0-beta4
+rem set the runtime path because the above commands set \.dnx<space>\runtimes
+set PATH=!USERPROFILE!\.dnx\runtimes\dnx-clr-win-x86.1.0.0-beta4\bin;!PATH!
+
+call dnu restore
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd tests\LightningDB.Tests
+call dnx . test
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd ..\..
+call dnu publish src\LightningDB --no-source --out artifacts\build\LightningDB --runtime dnx-clr-win-x86.1.0.0-beta4
+
+popd
