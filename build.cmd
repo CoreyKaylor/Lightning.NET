87e3e7d623ab05b423edf650939bb267e858b12b
Corey Kaylor <corey@kaylors.net>
2015-06-05 20:28:14 -0600

    Trying out some changes to fix build

--- build.cmd
+++ build.cmd
@@ -6,8 +6,8 @@ setlocal EnableDelayedExpansion
 where dnvm
 if %ERRORLEVEL% neq 0 (
     @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
-    set PATH=!PATH!;!userprofile!\.k\bin
-    set KRE_HOME=!USERPROFILE!\.k
+    set PATH=!PATH!;!userprofile!\.dnx\bin
+    set DNX_HOME=!USERPROFILE!\.dnx
     goto install
 )
 
