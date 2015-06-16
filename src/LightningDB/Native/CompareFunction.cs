a4943bc0642ca431ab001ea33c12e14e061c6b73
Corey Kaylor <corey@kaylors.net>
2015-06-15 19:08:10 -0600

    Checkpoint: Simplified CompareDelegate

--- src/LightningDB/Native/CompareFunction.cs
+++ src/LightningDB/Native/CompareFunction.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
+﻿using System.Runtime.InteropServices;
 
 namespace LightningDB.Native
 {
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    delegate int CompareFunction(
-        IntPtr left,
-        IntPtr right);
+    delegate int CompareFunction(ref ValueStructure left, ref ValueStructure right);
 }
