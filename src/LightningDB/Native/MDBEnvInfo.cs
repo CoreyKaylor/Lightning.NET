167afbbc41dc77baba9d55ec0db9d96ee3f17d27
Corey Kaylor <corey@kaylors.net>
2015-06-30 13:09:00 -0600

    Structs are already sequential

--- src/LightningDB/Native/MDBEnvInfo.cs
+++ src/LightningDB/Native/MDBEnvInfo.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 
 namespace LightningDB.Native
 {
-    [StructLayout(LayoutKind.Sequential)]
     struct MDBEnvInfo
     {
         /// <summary>
