424af4e29dd0e7522264230dd351b746c65770e0
Corey Kaylor <corey@kaylors.net>
2016-05-20 15:17:15 -0700

    No prompt for install

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if ! type dotnet > /dev/null 2>&1; then
     sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
     sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
     sudo apt-get update
-    sudo apt-get install dotnet-dev-1.0.0-preview1-002702
+    sudo apt-get -y install dotnet-dev-1.0.0-preview1-002702
 fi
 
 type make >/dev/null 2>&1 || { echo >&2 "Can't find dependency 'make' for lmdb native lib compile.  Aborting."; exit 1; }
