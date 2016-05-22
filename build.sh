7ce54d418e4a93bb9d50a8337576280b387bda38
Corey Kaylor <corey@kaylors.net>
2016-05-22 07:55:37 -0700

    Trying alternative way to install dotnet on linux

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 rm -rf artifacts
 if ! type dotnet > /dev/null 2>&1; then
-    sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
-    sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
-    sudo apt-get update
-    sudo apt-get -y install dotnet-dev-1.0.0-preview1-002702
+    curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview1/scripts/obtain/dotnet-install.sh | bash /dev/stdin --version 1.0.0-preview1-002702 --install-dir ~/dotnet
+    sudo ln -s ~/dotnet/dotnet /usr/local/bin
 fi
 
 type make >/dev/null 2>&1 || { echo >&2 "Can't find dependency 'make' for lmdb native lib compile.  Aborting."; exit 1; }
