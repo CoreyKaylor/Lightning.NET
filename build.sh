c02e779c568244ffcc33698044b70ca2799e812f
Corey Kaylor <corey@kaylors.net>
2017-03-11 15:04:58 -0700

    Dotnet upgrade (#88)
    Updated project to new / old csproj format and dotnet cli tooling

--- build.sh
+++ build.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-rm -rf artifacts
-if ! type dotnet > /dev/null 2>&1; then
-    curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.sh | bash /dev/stdin --version 1.0.0-preview2-003121 --install-dir ~/dotnet
-    sudo ln -s ~/dotnet/dotnet /usr/local/bin
-fi
-
 type make >/dev/null 2>&1 || { echo >&2 "Can't find dependency 'make' for lmdb native lib compile.  Aborting."; exit 1; }
 type gcc >/dev/null 2>&1 || { echo >&2 "Can't find dependency 'gcc' for lmdb native lib compile.  Aborting."; exit 1; }
 
 cd mdb/libraries/liblmdb/
 make
 cd ../../../
-cd workaround/LibLmdb
-dotnet restore
-dotnet pack --configuration Release --output ../../artifacts
-cd ../../src/LightningDB.Tests
-
-dotnet restore
+dotnet restore src/Lightning.Net.sln
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-LD_LIBRARY_PATH=../../mdb/libraries/liblmdb/:$LD_LIBRARY_PATH dotnet test -f netcoreapp1.0
+cd src/LightningDB.Tests
+dotnet build -f netcoreapp1.1
+mv ../../mdb/libraries/liblmdb/liblmdb.so bin/Debug/netcoreapp1.1/liblmdb.so
+ls bin/Debug/netcoreapp1.1/
+dotnet test -f netcoreapp1.1
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
