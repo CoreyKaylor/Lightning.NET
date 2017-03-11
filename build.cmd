4c4a99267802caf35414323fe1a2f36d502728d0
Corey Kaylor <corey@kaylors.net>
2017-03-11 13:11:44 -0700

    Fixing build file and nuget pack

--- build.cmd
+++ build.cmd
@@ -3,18 +3,12 @@
 pushd %~dp0
 
 md artifacts
-cd workaround/LibLmdb
-call dotnet restore
-call dotnet pack --configuration Release --output ..\..\artifacts
-cd ..\..\
-call dotnet restore
+call dotnet restore src/Lightning.NET.sln
 if %errorlevel% neq 0 exit /b %errorlevel%
-call dotnet test src/LightningDB.Tests
+call dotnet test src/LightningDB.Tests/LightningDB.Tests.csproj
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo Packing LightningDb Nuget
-call dotnet pack src/LightningDB --configuration Release --output artifacts
-del artifacts\LibLmdb.0.0.1.nupkg
-del artifacts\LibLmdb.0.0.1.symbols.nupkg
+call dotnet pack src/LightningDB --configuration Release --output ..\..\artifacts
 
 popd
