e52fa536c09ee38afe366e74be26f7a483418e20
Corey Kayor <corey@kaylors.net>
2017-03-11 14:29:32 -0700

    Going around LD_LIBRARY_PATH

--- build.sh
+++ build.sh
@@ -4,11 +4,9 @@ type gcc >/dev/null 2>&1 || { echo >&2 "Can't find dependency 'gcc' for lmdb nat
 
 cd mdb/libraries/liblmdb/
 make
-mv liblmdb.so lmdb.so
 cd ../../../
 dotnet restore src/Lightning.Net.sln
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-cd src/LightningDB.Tests
-LD_LIBRARY_PATH=../../mdb/libraries/liblmdb/:$LD_LIBRARY_PATH
-dotnet test -f netcoreapp1.1
+cp mdb/libraries/liblmdb/liblmdb.so src/LightningDB.Tests/bin/Debug/netcoreapp1.1
+dotnet test src/LightningDB.Tests/LightningDB.Tests.csproj -f netcoreapp1.1
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
