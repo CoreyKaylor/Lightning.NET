b7c56437acd19631c9a432bef818ac79417a225b
Matt Little <zone117x@gmail.com>
2018-01-17 18:01:42 -0700

    Update build.sh to create lmdb.dylib on MacOS and to target netcoreapp2.0

--- build.sh
+++ build.sh
@@ -4,12 +4,19 @@ type gcc >/dev/null 2>&1 || { echo >&2 "Can't find dependency 'gcc' for lmdb nat
 
 cd mdb/libraries/liblmdb/
 make
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    gcc -dynamiclib -o lmdb.dylib mdb.o midl.o
+fi
 cd ../../../
 dotnet restore src/Lightning.Net.sln
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd src/LightningDB.Tests
-dotnet build -f netcoreapp1.1
-mv ../../mdb/libraries/liblmdb/liblmdb.so bin/Debug/netcoreapp1.1/liblmdb.so
-ls bin/Debug/netcoreapp1.1/
-dotnet test -f netcoreapp1.1
+dotnet build -f netcoreapp2.0
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    mv ../../mdb/libraries/liblmdb/lmdb.dylib bin/Debug/netcoreapp2.0/lmdb.dylib
+else
+    mv ../../mdb/libraries/liblmdb/liblmdb.so bin/Debug/netcoreapp2.0/liblmdb.so
+fi
+ls bin/Debug/netcoreapp2.0/
+dotnet test -f netcoreapp2.0
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
